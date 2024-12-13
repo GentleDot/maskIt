28b97e4d164a8996948852c3ab8a9c4cf02b78d1
gentledot <gentledot.wp@gmail.com>
2024-12-13 20:59:34 +0900

    test(module test added): module 가져온 뒤 마스킹 테스트

--- maskit/src/test/java/net/gentledot/maskit/DataMaskingTest.java
+++ maskit/src/test/java/net/gentledot/maskit/DataMaskingTest.java
@@ -70,4 +70,58 @@ class DataMaskingTest {
     void failTest_MaskWhenInvalidRuleKeyThenThrowException() {
         assertThrows(MaskingServiceException.class, () -> dataMasking.getModule(null));
     }
+
+    @DisplayName("CREDIT_CARD 데이터 타입의 마스킹 모듈 테스트")
+    @Test
+    void testCreditCardModuleMaskedCreditCard() {
+        String data = "1234567812345678";
+        String expectedMaskedData = "1234****5678";
+        String actualMaskedData = dataMasking.getModule(DataTypes.CREDIT_CARD).mask(data);
+        assertEquals(expectedMaskedData, actualMaskedData);
+    }
+
+    @DisplayName("SSN 데이터 타입의 마스킹 모듈 테스트")
+    @Test
+    void testSSNModuleMaskedSSN() {
+        String data = "001212-1234567";
+        String expectedMaskedData = "001212-*******";
+        String actualMaskedData = dataMasking.getModule(DataTypes.SSN).mask(data);
+        assertEquals(expectedMaskedData, actualMaskedData);
+    }
+
+    @DisplayName("EMAIL 데이터 타입의 마스킹 모듈 테스트")
+    @Test
+    void testEmailModuleMaskedEmail() {
+        String data = "example@example.com";
+        String expectedMaskedData = "e***@example.com";
+        String actualMaskedData = dataMasking.getModule(DataTypes.EMAIL).mask(data);
+        assertEquals(expectedMaskedData, actualMaskedData);
+    }
+
+    @DisplayName("PHONE_NUMBER 데이터 타입의 마스킹 모듈 테스트")
+    @Test
+    void testPhoneNumberModuleMaskedPhoneNumber() {
+        String data = "01012345678";
+        String expectedMaskedData = "010****5678";
+        String actualMaskedData = dataMasking.getModule(DataTypes.PHONE_NUMBER).mask(data);
+        assertEquals(expectedMaskedData, actualMaskedData);
+    }
+
+    @DisplayName("NAME 데이터 타입의 마스킹 모듈 테스트")
+    @Test
+    void testNameModuleMaskedName() {
+        String data = "홍길동";
+        String expectedMaskedData = "홍*";
+        String actualMaskedData = dataMasking.getModule(DataTypes.NAME).mask(data);
+        assertEquals(expectedMaskedData, actualMaskedData);
+    }
+
+    @DisplayName("ADDRESS 데이터 타입의 마스킹 모듈 테스트")
+    @Test
+    void testAddressModuleMaskedAddress() {
+        String data = "서울특별시 강남구 테헤란로7길 123";
+        String expectedMaskedData = "서울특별시 강남구 **** 123";
+        String actualMaskedData = dataMasking.getModule(DataTypes.ADDRESS).mask(data);
+        assertEquals(expectedMaskedData, actualMaskedData);
+    }
 }
