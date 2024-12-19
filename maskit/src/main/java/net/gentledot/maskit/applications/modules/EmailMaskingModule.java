7b4e7879dd40db05142eb5828bbbbb8430efca8b
gentledot <gentledot.wp@gmail.com>
2024-12-19 23:12:00 +0900

    feat(마스킹 기능 재구현): EmailMaskingModule의 마스킹 기능 재구현 및 테스트
    - maskIndex로 대체

--- maskit/src/main/java/net/gentledot/maskit/applications/modules/EmailMaskingModule.java
+++ maskit/src/main/java/net/gentledot/maskit/applications/modules/EmailMaskingModule.java
@@ -1,8 +1,6 @@
 package net.gentledot.maskit.applications.modules;
 
 import net.gentledot.maskit.exceptions.ExceptionHandler;
-import net.gentledot.maskit.exceptions.MaskingServiceException;
-import net.gentledot.maskit.exceptions.ServiceError;
 import net.gentledot.maskit.models.util.EmailRegexUtil;
 import net.gentledot.maskit.models.vaildator.DataValidator;
 import net.gentledot.maskit.models.vaildator.EmailValidator;
@@ -58,14 +56,7 @@ public class EmailMaskingModule extends MaskitMaskingModule implements MaskingMo
 
     @Override
     public String mask(String data, int fromIndex, int toIndex) {
-        if (super.isEmpty(data) || fromIndex < 0 || toIndex > data.length() || fromIndex >= toIndex) {
-            throw new MaskingServiceException(ServiceError.MASKING_INVALID_REQUEST);
-        }
-        StringBuilder masked = new StringBuilder(data);
-        for (int i = fromIndex; i < toIndex; i++) {
-            masked.setCharAt(i, '*');
-        }
-        return masked.toString();
+        return super.maskIndex(data, fromIndex, toIndex);
     }
 
     @Override
