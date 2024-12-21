f6f64fe983e90bb9136bcde000e3df105af1b164
gentledot <gentledot.wp@gmail.com>
2024-12-21 10:23:07 +0900

    feat(마스킹 기능 재구현): PhoneNumberMaskingModule 마스킹 기능 재구현 및 테스트
    - 전화번호 형식 validation

--- maskit/src/main/java/net/gentledot/maskit/applications/modules/PhoneNumberMaskingModule.java
+++ maskit/src/main/java/net/gentledot/maskit/applications/modules/PhoneNumberMaskingModule.java
@@ -1,11 +1,12 @@
 package net.gentledot.maskit.applications.modules;
 
-import net.gentledot.maskit.exceptions.ExceptionHandler;
 import net.gentledot.maskit.exceptions.MaskingServiceException;
 import net.gentledot.maskit.exceptions.ServiceError;
+import net.gentledot.maskit.models.util.PhoneNumberRegexUtil;
 import net.gentledot.maskit.models.vaildator.DataValidator;
 import net.gentledot.maskit.models.vaildator.PhoneNumberValidator;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PhoneNumberMaskingModule extends MaskitMaskingModule implements MaskingModule {
@@ -27,38 +28,53 @@ public class PhoneNumberMaskingModule extends MaskitMaskingModule implements Mas
     @Override
     public String mask(String data) {
         validator.isValid(data);
-        try {
-            return data.replaceAll("(\\d{3})\\d{4}(\\d{4})", "$1****$2");
-        } catch (Exception e) {
-            ExceptionHandler.handleException(e, "masking error occurred.");
-        }
-        return data;
-    }
 
-    @Override
-    public String mask(String data, int fromIndex, int toIndex) {
-        if (super.isEmpty(data) || fromIndex < 0 || toIndex > data.length() || fromIndex > toIndex) {
+        Matcher matcher = PhoneNumberRegexUtil.PHONE_NUMBER_PATTERN.matcher(data);
+
+        StringBuilder masked = new StringBuilder(data);
+        int start = -1;
+        int end = -1;
+
+        // Find the start and end indices of the line number
+        if (matcher.find()) {
+            start = matcher.start(4); // Start index of the line number
+            end = matcher.end(4); // End index of the line number
+        }
+
+        if (start == -1 || end == -1) {
             throw new MaskingServiceException(ServiceError.MASKING_INVALID_REQUEST);
         }
-        StringBuilder masked = new StringBuilder(data);
-        for (int i = fromIndex; i < toIndex; i++) {
-            masked.setCharAt(i, '*');
+
+        for (int i = 0; i < start; i++) {
+            if (Character.isDigit(masked.charAt(i))) {
+                masked.setCharAt(i, '*');
+            }
         }
+
         return masked.toString();
     }
 
     @Override
+    public String mask(String data, int fromIndex, int toIndex) {
+        validator.isValid(data);
+        return super.maskIndex(data, fromIndex, toIndex);
+    }
+
+    @Override
     public String maskFront(String data, int length) {
+        validator.isValid(data);
         return super.maskFront(data, length);
     }
 
     @Override
     public String maskBack(String data, int length) {
+        validator.isValid(data);
         return super.maskBack(data, length);
     }
 
     @Override
     public String maskWithRegex(String data, Pattern regex) {
+        validator.isValid(data);
         return super.maskWithRegex(data, regex);
     }
 }
--- maskit/src/main/java/net/gentledot/maskit/models/vaildator/PhoneNumberValidator.java
+++ maskit/src/main/java/net/gentledot/maskit/models/vaildator/PhoneNumberValidator.java
@@ -8,9 +8,15 @@ import net.gentledot.maskit.models.util.PhoneNumberRegexUtil;
 public class PhoneNumberValidator implements DataValidator {
 
     public boolean isValid(String phoneNumber) {
-        if (phoneNumber == null) {
+        if (isEmpty(phoneNumber)) {
             ExceptionHandler.handleException(new MaskingServiceException(ServiceError.MASKING_INVALID_REQUEST), "null phone number is not allowed.");
         }
-        return PhoneNumberRegexUtil.PHONE_NUMBER_PATTERN.matcher(phoneNumber).matches();
+        boolean matches = PhoneNumberRegexUtil.PHONE_NUMBER_PATTERN.matcher(phoneNumber).matches();
+
+        if (!matches) {
+            ExceptionHandler.handleException(new MaskingServiceException(ServiceError.MASKING_INVALID_REQUEST), "Invalid phone number format.");
+        }
+
+        return true;
     }
 }
