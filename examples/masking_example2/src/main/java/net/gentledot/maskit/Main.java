442787d7e56cc7fe9ea3e908168c11e798b60cb9
gentledot <gentledot.wp@gmail.com>
2024-12-21 17:19:13 +0900

    docs(custom Module 사용하여 마스킹 구성 로직 예시 추가):
    - 0.0.6을 사용

--- examples/masking_example2/src/main/java/net/gentledot/maskit/Main.java
+++ examples/masking_example2/src/main/java/net/gentledot/maskit/Main.java
@@ -1,11 +1,22 @@
 package net.gentledot.maskit;
 
+import net.gentledot.maskit.applications.modules.MaskingModule;
 import net.gentledot.maskit.custom.CustomAddressMaskingModule;
+import net.gentledot.maskit.models.DataTypes;
 
 public class Main {
     public static void main(String[] args) {
         DataMasking masking = DataMasking.builder()
                 .addressMaskingModule(new CustomAddressMaskingModule())
                 .build(); // address module을 별도 생성 후 구성
+
+        MaskingModule addressMaskingModule = masking.getModule(DataTypes.ADDRESS);
+
+        // 주소 마스킹 수행
+        String address = "123 Main St, Springfield, IL 62704";
+        String maskedAddress = addressMaskingModule.mask(address);
+
+        // 결과 출력
+        System.out.println("Masked Address: " + maskedAddress); // *** Main St, Springfield, IL *****
     }
 }
